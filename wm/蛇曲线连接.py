ec7168c5d2f7e5558353c068f00e71794b9a404b
642529689 <642529689@qq.com>
2023-10-09 21:04:26 +0800

    1009

--- wm/蛇曲线连接.py
+++ wm/蛇曲线连接.py
@@ -3,7 +3,7 @@ import maya.cmds as cmds
 def ysNewConnect(*args):
     print 'Button 1 was pushed.'
     
-def ysNewConnect(*args):
+def ysDefConnect(*args):
     print 'Button 1 was pushed.'
   
 if cmds.window('connectWin', exists=True):
@@ -12,6 +12,6 @@ if cmds.window('connectWin', exists=True):
 cmds.window('connectWin', width=150 )
 cmds.columnLayout( adjustableColumn=True )
 cmds.button( label='创建新链接', command=ysNewConnect )
-cmds.button( label='恢复默认', command=defaultButtonPush )
+cmds.button( label='恢复默认', command=ysDefConnect )
 
 cmds.showWindow('connectWin')
