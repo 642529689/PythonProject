49e6d6c58e70838e5ef580f611db09181bd999ed
642529689 <642529689@qq.com>
2023-10-10 14:33:02 +0800

    1010

--- wm/蛇曲线连接.py
+++ wm/蛇曲线连接.py
@@ -1,10 +1,34 @@
 import maya.cmds as cmds
 
 def ysNewConnect(*args):
-    print 'Button 1 was pushed.'
+    sel = cmds.ls(sl=True)
+    if len(sel) != 2:
+        cmds.warning('请先选择自己创建的曲线 再加选 "All_Con" 控制器')
+        return
+    
+    defCurve = sel[1].split(':')
+    if len(defCurve) == 1:
+        defCurve = 'pathCurve'
+    else:
+        defCurve = '%s:pathCurve' % defCurve[0]
+
+    cmds.connectAttr(sel[0]+'.worldSpace[0]', defCurve+'.create', force = True)
     
 def ysDefConnect(*args):
-    print 'Button 1 was pushed.'
+    sel = cmds.ls(sl=True)
+    if len(sel) != 1:
+        cmds.warning('请选择 "All_Con" 控制器')
+        return
+    
+    defCurve = sel[0].split(':')
+    if len(defCurve) == 1:
+        oldCurve = 'oldCurve'
+        defCurve = 'pathCurve'        
+    else:
+        oldCurve = '%s:oldCurve'% defCurve[0]
+        defCurve = '%s:pathCurve'% defCurve[0]        
+
+    cmds.connectAttr(oldCurve+'.worldSpace[0]', defCurve+'.create', force = True)
   
 if cmds.window('connectWin', exists=True):
     cmds.deleteUI('connectWin')
